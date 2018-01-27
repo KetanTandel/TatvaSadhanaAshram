3ec12ced4eb3e69bfff32052c094bb2ba1222a24
KetanTandel <31986610+KetanTandel@users.noreply.github.com>
2018-01-27 13:25:32 +0530

    angular implement
    New Angular Project add

---
+++ TatvaShadhana New/js/rootScript.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.min.js" />
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/home", {
+        templateUrl: "gujarati/home.html"
+    })
+    .when("/AboutUs", {
+        templateUrl: "gujarati/AboutUs.html"
+    })
+    //.when("/paris", {
+    //    templateUrl: "paris.htm"
+    //});
+});
