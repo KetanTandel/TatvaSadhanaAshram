27b7cbb3eae9fdaac3055fbff9cf8ed4ea49982b
KetanTandel <31986610+KetanTandel@users.noreply.github.com>
2018-01-28 16:05:20 +0530

    28-01-2018

--- TatvaShadhana New/js/rootScript.js
+++ TatvaShadhana New/js/rootScript.js
@@ -8,6 +8,30 @@ app.config(function ($routeProvider) {
     .when("/AboutUs", {
         templateUrl: "gujarati/AboutUs.html"
     })
+    .when("/ContactUs", {
+        templateUrl: "gujarati/ContactUs.html"
+    })
+    .when("/Sansodhan", {
+        templateUrl: "gujarati/Sansodhan.html"
+    })
+    .when("/JyotishShastra", {
+        templateUrl: "gujarati/JyotishShastra.html"
+    })
+    .when("/BalKelavani", {
+        templateUrl: "gujarati/BalKelavani.html"
+    })
+    .when("/Yog", {
+        templateUrl: "gujarati/Yog.html"
+    })
+    .when("/Ayurved", {
+        templateUrl: "gujarati/Ayurved.html"
+    })
+    .when("/Pranayam", {
+        templateUrl: "gujarati/Pranayam.html"
+    })
+    .otherwise("/home", {
+        templateUrl: "gujarati/home.html"
+    })
     //.when("/paris", {
     //    templateUrl: "paris.htm"
     //});
